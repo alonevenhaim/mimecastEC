00f53236db5e0fd312bf84ea7359128883009fd5
alonevenhaim <alonevenhaim@gmail.com>
2023-11-23 21:40:03 +0200

    Add cypress configuration files

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'xx4wov',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
